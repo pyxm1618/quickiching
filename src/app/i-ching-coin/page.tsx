e7551d3cab306c21538f22b48439562c7ee29271
pyxm1618 <146602164+pyxm1618@users.noreply.github.com>
2026-08-01 20:47:45 +0800

    Use brand-neutral legacy route identifier

--- src/app/i-ching-coin/page.tsx
+++ src/app/i-ching-coin/page.tsx
@@ -1,5 +1,5 @@
 import { permanentRedirect } from "next/navigation";
 
-export default function LegacyIChingCoinRoute() {
+export default function LegacyCoinKeywordRoute() {
   permanentRedirect("/quick-i-ching");
 }
