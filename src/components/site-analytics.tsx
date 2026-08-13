b9098fd63b625df0bb1a6788dc51d7c52b57b7fa
pyxm1618 <146602164+pyxm1618@users.noreply.github.com>
2026-08-13 10:51:39 +0800

    feat: integrate GA4 and Microsoft Clarity

---
+++ src/components/site-analytics.tsx
@@ -0,0 +1,38 @@
+import Script from "next/script";
+import { GoogleAnalyticsPageView } from "@/components/google-analytics-page-view";
+import { getAnalyticsConfig } from "@/lib/analytics";
+
+function GoogleAnalytics({ measurementId }: { measurementId: string }) {
+  return (
+    <>
+      <Script id="google-analytics-bootstrap" strategy="beforeInteractive">
+        {`window.dataLayer=window.dataLayer||[];window.gtag=function(){window.dataLayer.push(arguments);};window.gtag('js',new Date());window.gtag('config','${measurementId}',{send_page_view:false});`}
+      </Script>
+      <Script
+        id="google-analytics-loader"
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <GoogleAnalyticsPageView measurementId={measurementId} />
+    </>
+  );
+}
+
+function MicrosoftClarity({ projectId }: { projectId: string }) {
+  return (
+    <Script id="microsoft-clarity" strategy="afterInteractive">
+      {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments);};t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,'clarity','script','${projectId}');`}
+    </Script>
+  );
+}
+
+export function SiteAnalytics() {
+  const { gaMeasurementId, clarityProjectId } = getAnalyticsConfig();
+
+  return (
+    <>
+      {gaMeasurementId ? <GoogleAnalytics measurementId={gaMeasurementId} /> : null}
+      {clarityProjectId ? <MicrosoftClarity projectId={clarityProjectId} /> : null}
+    </>
+  );
+}
