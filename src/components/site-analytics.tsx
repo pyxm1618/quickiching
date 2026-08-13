b5a2e50f83259aa71e337243fd9448cb67cea273
pyxm1618 <146602164+pyxm1618@users.noreply.github.com>
2026-08-13 11:22:24 +0800

    perf: defer analytics providers until browser idle

--- src/components/site-analytics.tsx
+++ src/components/site-analytics.tsx
@@ -14,7 +14,7 @@ function GoogleAnalytics({ measurementId }: { measurementId: string }) {
       <Script
         id="google-analytics-loader"
         src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
       <GoogleAnalyticsPageView measurementId={measurementId} />
     </>
@@ -23,7 +23,7 @@ function GoogleAnalytics({ measurementId }: { measurementId: string }) {
 
 function MicrosoftClarity({ projectId }: { projectId: string }) {
   return (
-    <Script id="microsoft-clarity" strategy="afterInteractive">
+    <Script id="microsoft-clarity" strategy="lazyOnload">
       {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments);};c[a]('consentv2',{ad_Storage:'denied',analytics_Storage:'denied'});t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,'clarity','script','${projectId}');`}
     </Script>
   );
